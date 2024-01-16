215cbcbad97065d52ab728fdee89ce2d13c875bb
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-16 11:25:36 +0530

    Fix Bug -Total expense
    Handle the side effect of updating total expense using use effect rather than dispatching in different functions

--- src/pages/Home/ExpenseShow.jsx
+++ src/pages/Home/ExpenseShow.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ExpenseCard from './ExpenseCard';
+import { updateTotalExpense } from '../../services/store/slices/expenseSlice';
 
 function ExpenseShow() {
   const { expenses ,loading} = useSelector(state => state.expense);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(updateTotalExpense());
+  },[expenses])
 
   return (
     <div className="relative max-w-[77rem] min-h-[20rem] p-4 bg-base-100 mx-auto mt-10 shadow-xl rounded-lg flex items-start justify-center gap-4 flex-wrap">
